8fd3338d56afe0ef9d902afb1f3053ff4e9a5eb1
Ladicle <Ladiclexxx@gmail.com>
2017-01-15 03:42:04 +0900

    Impl key event viewer

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gvalkov/golang-evdev"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		fmt.Println("You must specify device name.")
+		os.Exit(1)
+	}
+
+	dev, err := evdev.Open(os.Args[1])
+	if err != nil {
+		fmt.Printf("Unable to open input device: %s\n", os.Args[1])
+		os.Exit(1)
+	}
+
+	fmt.Printf("Listening for evens of %s.\n", os.Args[1])
+	for {
+		events, err := dev.Read()
+		if err != nil {
+			fmt.Printf("Can not read device envets: %v\n", err)
+			continue
+		}
+		for i := range events {
+			str, err := detectDownKeyEvent(&events[i])
+			if err != nil {
+				continue
+			}
+			fmt.Println(str)
+		}
+	}
+}
+
+func detectDownKeyEvent(ev *evdev.InputEvent) (string, error) {
+	if ev.Value != 1 {
+		return "", fmt.Errorf("Value is not 1: %d", ev.Value)
+	}
+
+	var codeName string
+	code := int(ev.Code)
+
+	val, hashkey := evdev.KEY[code]
+	if hashkey {
+		codeName = val
+	} else {
+		val, hashkey := evdev.BTN[code]
+		if hashkey {
+			codeName = val
+		} else {
+			codeName = "?"
+		}
+	}
+	return codeName, nil
+}
